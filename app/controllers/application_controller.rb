ec6edf0ad0acfb6875b221c0def3e5b94c409e7c
Ayumu3746221 <a.kkt0307@gmail.com>
2025-04-06 10:42:17 +0900

    create user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user!
+
+  private
+  
+  def authenticate_user!
+    if request.headers['Authorization'].present?
+      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, Rails.application.credentials.devise_jwt_secret_key!).first
+      @current_user_id = jwt_payload['sub']
+    else
+      render json: { error: 'Authorization header is missing' }, status: :unauthorized
+    end
+  rescue JWT::DecodeError
+    render json: { error: 'Invalid token' }, status: :unauthorized
+  end
+  
+  def current_user
+    @current_user ||= User.find(@current_user_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { parent: "parent" , child: "child" }
+  validates :role, presence: true, inclusion: { in: roles.keys }
 end
